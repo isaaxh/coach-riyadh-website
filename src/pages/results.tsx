a9cd85e954b7b2bb186c1ea7d9c694389f1ab6a4
Isaac Hussain <isaaclearns58@gmail.com>
2023-08-01 19:18:25 +0300

    add layout component to all the pages

---
+++ src/pages/results.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Layout from "../components/Layout";
+
+const results = () => {
+  return (
+    <Layout>
+      <div>results</div>;
+    </Layout>
+  );
+};
+
+export default results;
---
+++ src/pages/services.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Layout from "../components/Layout";
+
+const services = () => {
+  return (
+    <Layout>
+      <div>services</div>;
+    </Layout>
+  );
+};
+
+export default services;
